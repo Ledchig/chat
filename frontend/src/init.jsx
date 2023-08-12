a1fb43ce09db05c10c8570afea3b9339917c3bc3
Ledchig <s9826059221@gmail.com>
2023-08-12 22:28:18 +0500

    move sockets in init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux';
 import i18n from 'i18next';
 import leoProfanity from 'leo-profanity';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import io from 'socket.io-client';
 import resources from './locales/index';
 import { SocketContext } from './context/index';
-import sockets from './sockets';
 import App from './App';
 import store from './slices/index';
+import {
+  addChannel, removeChannel, renameChannel,
+} from './slices/channelsSlice';
+import { addMessage } from './slices/messagesSlice';
 
 const init = () => {
   const censorshipDictionaryRu = leoProfanity.getDictionary('ru');
@@ -22,6 +26,63 @@ const init = () => {
     resources,
     fallbackLng: 'ru',
   });
+
+  const socket = io();
+
+  socket.on('newMessage', (payload) => {
+    store.dispatch(addMessage(payload));
+  });
+
+  socket.on('newChannel', (payload) => {
+    store.dispatch(addChannel(payload));
+  });
+
+  socket.on('removeChannel', (payload) => {
+    store.dispatch(removeChannel(payload));
+  });
+
+  socket.on('renameChannel', (payload) => {
+    store.dispatch(renameChannel(payload));
+  });
+
+  const sockets = {
+    sendMessage: (message) => new Promise((resolve, reject) => {
+      socket.emit('newMessage', message, (response) => {
+        if (response.status === 'ok') {
+          resolve(response.data);
+        } else {
+          reject();
+        }
+      });
+    }),
+    addChannel: (channel) => new Promise((resolve, reject) => {
+      socket.emit('newChannel', channel, (response) => {
+        if (response.status === 'ok') {
+          resolve(response.data);
+        } else {
+          reject();
+        }
+      });
+    }),
+    removeChannel: (id) => new Promise((resolve, reject) => {
+      socket.emit('removeChannel', id, (response) => {
+        if (response.status === 'ok') {
+          resolve(response.data);
+        } else {
+          reject();
+        }
+      });
+    }),
+    renameChannel: (channel) => new Promise((resolve, reject) => {
+      socket.emit('renameChannel', channel, (response) => {
+        if (response.status === 'ok') {
+          resolve(response.data);
+        } else {
+          reject();
+        }
+      });
+    }),
+  };
 
   return (
     <RollbarProvider config={rollbarConfig}>
